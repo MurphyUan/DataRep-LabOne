dcd3bf0ad7d7f23e3119b6387e7b5284a167ce45
UAN MURPHY - STUDENT <G00385338@gmit.ie>
2021-09-27 14:34:27 +0100

    Lab Upload - Commenting

--- bmi.js
+++ bmi.js
@@ -1,15 +1,22 @@
+//BMI Class
 class BMI{
+    //Constructor with weight and height parameters
     constructor(height, weight){
+        //Declare variables
         this.height = height;
         this.weight = weight;
     }
-
+    // Calculate the Body Mass Index (BMI)using weight and height variables
     calculateBMI(){
+        //Calculate BMI
         let bmi = this.weight/(this.height**2);
+        //Return Calculated BMI value
         return bmi;
     }
 }
-
+//instantiate object with parameters
 let myBMI = new BMI(2,98);
-let calc  =myBMI.calculateBMI();
-console.log(calc);
+//Calculate BMI value
+let calc = myBMI.calculateBMI();
+//Log result to the screen
+console.log(`BMI: ${calc}`);
--- cars.js
+++ cars.js
@@ -1,29 +1,40 @@
+//Vehicle Class
 class Vehicle{
+    //Constructor with make, model and year parameters
     constructor(make, model, year){
+        //Declare variables
         this.make = make;
         this.model = model;
         this.year = year;
     }
-
+    // Log information to the console / screen
     Information(){
         console.log(`Make: ${this.make}\nModel: ${this.model}\nYear: ${this.year}`);
     }
 }
-
+//instantiate object with parameters
 let myVehicle = new Vehicle("Audi","A3",1996);
+//display information about Vehicle object
 myVehicle.Information();
 
+//Cars Class, Child of Vehicle
 class Cars extends Vehicle{
+    //Constructor
     constructor(make, model, year, doors){
+        //Uses Vehicle Constructor
         super(make, model, year);
+        //Declare variables
         this.doors = doors;
     }
-
+    // Log information to the console / screen
     infoInformation(){
+        //Override Vehicle implementation
         super.Information();
+        //log to the screen
         console.log(`Number of Doors: ${this.doors}`);
     }
 }
-
+//instantiate object with additional parameters
 let myCar = new Cars("Audi","A3",1996, 4);
+//display information about Cars Object Child of Vehicle
 myCar.infoInformation();
